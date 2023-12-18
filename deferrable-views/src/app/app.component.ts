6979a99cbe1348fdd93aa97b1a26bb69bba7126b
joaovsj <joaosilva.jesus90@gmail.com>
2023-12-18 20:42:35 -0300

    Signal and Computed

--- deferrable-views/src/app/app.component.ts
+++ deferrable-views/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { DeferrableViewComponent } from './components/deferrable-view/deferrable-view.component';
+import { SignalsComponent } from './components/signals/signals.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, DeferrableViewComponent],
+  imports: [CommonModule, RouterOutlet, DeferrableViewComponent, SignalsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- deferrable-views/src/app/components/signals/signals.component.ts
+++ deferrable-views/src/app/components/signals/signals.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-signals',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './signals.component.html',
   styleUrl: './signals.component.scss'
 })
 export class SignalsComponent {
+
+  public firstName = signal("Joao ");
+  public lastName = signal("Jesus")
+
+  // Apenas receber o valor de um Signal não o faz REATIVO
+  public fullName = computed(() => {
+    return this.firstName() + this.lastName()
+  }) 
+
+
+  constructor(){
+    effect(()=>{
+      console.log(this.fullName());
+    });
+  }
+
+  public items = signal([1,2]);
+
+  public address = signal("Rua Martins Rosa de Novaes");
+
+  public showAddress(newAddress: string){
+
+    this.address.set(newAddress);
+    console.log(this.address());
+  }
+
+  public updateName(){
+    this.firstName.set("Pedro");
+  }
+
+  public updateArray(){
+
+    this.items.update((oldValues) => {
+      console.log(oldValues);
+      return [...oldValues, oldValues.length + 1]
+    })
+
+    // console.log(this.items().push(1));
+  }
 
 }
