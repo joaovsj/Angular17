dc0246488e582e61b66d126034f4a66b4ef1482e
joaovsj <joaosilva.jesus90@gmail.com>
2023-12-18 21:12:34 -0300

    Added assets

--- portfolio/src/app/modules/portfolio/pages/home/home.component.ts
+++ portfolio/src/app/modules/portfolio/pages/home/home.component.ts
@@ -1,10 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
+// COMPONENTS
+import { HeaderComponent } from '../../components/header/header.component';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule, 
+    HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
