4c4c2f917fb7ae07fdfc60e498ecaad2581bb4f4
joaovsj <joaosilva.jesus90@gmail.com>
2024-01-04 21:07:58 -0300

    Set locale

--- communication-between-components/src/app/components/pipes/angular-pipes/angular-pipes.component.ts
+++ communication-between-components/src/app/components/pipes/angular-pipes/angular-pipes.component.ts
@@ -1,12 +1,18 @@
-import { DatePipe, LowerCasePipe, UpperCasePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { CurrencyPipe, DatePipe, DecimalPipe, LowerCasePipe, PercentPipe, UpperCasePipe, registerLocaleData } from '@angular/common';
+import { Component, LOCALE_ID, signal } from '@angular/core';
+
+// Registrando dados da localidade
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt);
+
 
 @Component({
   selector: 'app-angular-pipes',
   standalone: true,
-  imports: [DatePipe, UpperCasePipe, LowerCasePipe],
+  imports: [DatePipe, UpperCasePipe, LowerCasePipe, CurrencyPipe, DecimalPipe, PercentPipe],
   templateUrl: './angular-pipes.component.html',
-  styleUrl: './angular-pipes.component.scss'
+  styleUrl: './angular-pipes.component.scss',
+  providers:  [{ provide: LOCALE_ID, useValue: 'pt-BR'}]
 })
 export class AngularPipesComponent {
 
