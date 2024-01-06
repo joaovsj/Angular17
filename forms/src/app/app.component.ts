755f3677e7ac9b1938ca2f7df9c9251c549c92da
joaovsj <joaosilva.jesus90@gmail.com>
2024-01-06 12:48:05 -0300

    Some changes done

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -2,14 +2,19 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+// Components
+import { TemplateDrivenFormsComponent } from './components/template-driven-forms/template-driven-forms.component';
+import { ReactiveFormsComponent } from './components/reactive-forms/reactive-forms.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, TemplateDrivenFormsComponent, ReactiveFormsComponent],
   template: `
     <router-outlet></router-outlet>
 
-
+    <app-template-driven-forms />
+    <!-- <app-reactive-forms /> -->
   `
 })
 export class AppComponent {
