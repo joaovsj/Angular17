c6a489c35330de67c4d0c9fdbbbb5cbca6dfac36
joaovsj <joaosilva.jesus90@gmail.com>
2024-01-22 22:27:30 -0300

    First routes

--- routes/src/app/app.routes.ts
+++ routes/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// COMPONENTS
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { JobsComponent } from './pages/jobs/jobs.component';
+
+export const routes: Routes = [
+    {
+        path: '', 
+        title: 'Início - Angular 17', // referente ao título da pagina
+        component: HomeComponent
+    },
+    {
+        path: 'sobre',
+        title: 'Sobre nós - Angular 17',
+        component: AboutComponent
+    },
+    {
+        path: 'nossos-servicos',
+        title: 'Serviços - Angular 17',
+        component: JobsComponent
+    }
+];
