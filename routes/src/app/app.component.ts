ae11f6567e1fe93c537db16220d6ce791fc3868a
joaovsj <joaosilva.jesus90@gmail.com>
2024-01-27 14:13:50 -0300

    Component opt-image created

--- routes/src/app/app.component.ts
+++ routes/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { OptImageComponent } from './components/opt-image/opt-image.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, OptImageComponent],
   template: `
-    <router-outlet></router-outlet>
+  <!-- <router-outlet></router-outlet>   -->
+    <app-opt-image/>
+    
   `,
 })
 export class AppComponent {
---
+++ routes/src/app/components/opt-image/opt-image.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-opt-image',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './opt-image.component.html',
+  styleUrl: './opt-image.component.scss'
+})
+export class OptImageComponent {
+
+}
