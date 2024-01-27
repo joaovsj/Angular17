e53b120336b3146ef8e18c393531775252bc502c
joaovsj <joaosilva.jesus90@gmail.com>
2024-01-27 10:32:09 -0300

    Some changes done

--- routes/src/app/pages/curso.routes.ts
+++ routes/src/app/pages/curso.routes.ts
@@ -6,13 +6,13 @@ export const pageRoutes: Routes = [
         path: '', 
         loadComponent: () => import('./home/home.component').then((p) => p.HomeComponent)
     },
-    // {
-    //     path: 'sobre', 
-    //     loadComponent: () => import('./about/about.component').then((p)=> p.AboutComponent)
-    // },
-    // {
-    //     path: 'servicos/:subject',
-    //     loadComponent: () => import('./jobs/jobs.component').then((p) => p.JobsComponent)
-    // } 
+    {
+        path: 'sobre', 
+        loadComponent: () => import('./about/about.component').then((p)=> p.AboutComponent)
+    },
+    {
+        path: 'servicos/:subject',
+        loadComponent: () => import('./jobs/jobs.component').then((p) => p.JobsComponent)
+    } 
 
 ];
