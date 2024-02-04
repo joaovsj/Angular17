81df919d30b0e5749026bf7c7ad1821299f59eca
joaovsj <joaosilva.jesus90@gmail.com>
2024-02-04 10:20:11 -0300

    some changes done

--- animations/src/app/components/test/test.component.ts
+++ animations/src/app/components/test/test.component.ts
@@ -10,18 +10,21 @@ import { Component, OnInit, signal } from '@angular/core';
   animations: [
     trigger('modal', [
       state('show', style({
-        opacity: 1
+        opacity: 1,
+        transform: 'translateY(0)',
+        pointerEvents: 'normal'
       })),
       state('hide', style({
-        opacity: 0
+        opacity: 0,
+        transform: 'translateY(50px)',
+        pointerEvents: 'none'
       })),
-
       transition('* <=> show', animate('.2s ease'))
     ])
   ]
 })
 export class TestComponent{
 
-  public visible = 'hide';
+  public visible = signal<string>('hide');
 
 }
